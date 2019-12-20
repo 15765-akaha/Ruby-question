bbed463cb97b6e65d25ebda2449772485c79d6d5
15765-akaha <turning2012@gmail.com>
2019-12-20 14:42:02 +0900

    [finish]2

---
+++ lesson4.rb
@@ -0,0 +1,11 @@
+name = "赤羽　諒"
+puts name
+puts "Rubyを学習"
+name = "鈴木さん"
+puts "Hello" + name
+puts "こんにちは#{name}"
+names = ["Perl", "Ruby", "Python", "Swift"]
+puts names[3]
+tall = {:太郎=>185, :二郎=>170, :花子=>150}
+puts tall[:花子]
+
